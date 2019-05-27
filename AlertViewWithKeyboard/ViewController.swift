93ce53d5a9af83b994139f2e1ccefabd88a49fde
Natalie <nwy0206@gmail.com>
2019-05-27 14:21:53 +0800

    Initial Commit

---
+++ AlertViewWithKeyboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlertViewWithKeyboard
+//
+//  Created by Natalie Ng on 2019/5/27.
+//  Copyright © 2019 appi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
